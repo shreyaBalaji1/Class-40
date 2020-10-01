8014f9812ed29acf854e3850f902f1308e5ee976
Shreya <123@gmail.com>
2020-10-01 18:02:12 -0500

    Animations

--- js/Game.js
+++ js/Game.js
@@ -3,6 +3,7 @@ class Game {
 
   }
 
+  //Reading gameState value from the database
   getState(){
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
@@ -30,9 +31,17 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1img);
+
     car2 = createSprite(300,200);
+    car2.addImage(car2img);
+
     car3 = createSprite(500,200);
+    car3.addImage(car3img);
+
     car4 = createSprite(700,200);
+    car4.addImage(car4img);
+
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +51,14 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
-      //var display_position = 100;
+      background("#c68767");
+      image(trackimg, 0, -displayHeight*4, displayWidth, displayHeight*5)
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +89,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3500) {
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+
+  end(){
+    console.log("Game Ended")
   }
 }
